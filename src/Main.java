355a84230ef4c3f20a54850733379ced2a6d2c07
Olga <oburenok@gmail.com>
2023-12-17 03:15:32 +0100

    Primera vercion del programa con seguimiento

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Scanner read = new Scanner(System.in);
+        int planta = 0;
+        int plantaSolicitada = 0;
+        boolean exit = false;
     }
 }
