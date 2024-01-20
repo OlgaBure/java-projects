31be8c229643089d06f45ad911476d13f588ec10
alumno <oburenok@gmail.com>
2024-01-20 19:09:36 +0100

    Segunda vercion del codigo del programa de ascensor

--- src/Alumnos/Tester.java
+++ src/Alumnos/Tester.java
@@ -1,10 +1,7 @@
 package Alumnos;
 
-import java.util.Scanner;
-
 public class Tester {
-    Scanner read = new Scanner(System.in);
     Profesor profesor = new Profesor();
-    System.out.println(Profesor.dameNota());
-    System.out.println(Profesor)
+    //System.out.println(Profesor.dameNota());
+
 }
